abf89932875d05278e02bab37783320b912a64e8
Emily <hendricks.emily7@gmail.com>
2025-10-01 06:57:49 -0400

    fix days to go

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -20,9 +20,13 @@ weddingNavLinksList.forEach(link => {
 });
 
 export const calculateDaysToGo = () => {
-    const weddingDate = new Date('2026-09-26T00:00:00'); // September 26, 2026
+    const weddingDate = new Date('2026-09-26');
     const today = new Date();
-    const diffTime = Math.abs(weddingDate.getTime() - today.getTime());
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    return `${diffDays} DAYS TO GO!`;
-  };
+    const todayUTC = Date.UTC(today.getFullYear(), today.getMonth(), today.getDate());
+    const weddingUTC = Date.UTC(weddingDate.getFullYear(), weddingDate.getMonth(), weddingDate.getDate());
+    const diffTime = Math.abs(weddingUTC - todayUTC);
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
+    return `${diffDays} DAYS TO GO!`; 
+}
+
